a2673f959640deabb7247b727a72115687ec1f53
Yoana <yoni.vasileva8b@gmail.com>
2020-02-06 18:06:23 +0200

    Add files via upload

--- neshto.php
+++ neshto.php
@@ -2,12 +2,74 @@
 <head>
 </head>
 <body>
-<h1>Connected to sql</h1>
+<h1 style="text-align: center">Crimes in the world</h1>
+
+<form action="neshto.php" method="get" style="text-align: center;">
+            <input type="submit" name="on" value="Show me avarage crimes">
+            <input type="submit" name="off" value="Show me sum of the murders">
+            <input type="submit" name="all" value="Show me all">
+        </form>
+
+<table style="text-align: center;">
 
 
-	<table align="center">
-		<caption>Crimes in the World</caption>
-		<tr>
+
+<?php
+$con = mysqli_connect('localhost','root','root','Crimes');
+
+	if(isset($_GET['on'])) {
+        onFunc($con);
+    }
+    if(isset($_GET['off'])) {
+        offFunc($con);
+    }
+    if(isset($_GET['all'])){
+    	allFunc($con);
+    }
+    	
+    
+
+    function onFunc($con){
+    	?>
+ 		<tr>
+			<th >Avarage crimes</th>
+		</tr>
+		<?php
+    	$opit1 = mysqli_query($con, "select AVG(violent_crime_total) as Alabama_avg from cities a where a.years >= 1960 AND a.years <= 1980");
+		while($row=mysqli_fetch_array($opit1)) {
+?>
+
+	<tr>
+ 		<td><?php echo $row['Alabama_avg']; ?></td>
+ 	
+	</tr>
+
+<?php }} ?>
+<?php
+
+ 	function offFunc($con){ 
+ 		?>
+ 		<tr>
+			<th>Sum of murders</th>
+		</tr>
+		<?php
+ 		$opit2 = mysqli_query($con, "select SUM(murder_rate) as Sum_mureder from cities a where a.city = 'Alabama';");
+		while($row=mysqli_fetch_array($opit2)) {
+?>
+
+	<tr>
+ 		<td><?php echo $row['Sum_mureder']; ?></td>
+ 	
+	</tr>
+
+<?php }} ?>
+
+
+<?php
+
+ 	function allFunc($con){ 
+ 		?>
+ 		<tr>
 			<th>City</th>
 			<th>Years</th>
 			<th>Population</th>
@@ -24,38 +86,38 @@
 			<th>Robbery rate</th>
 			<th>Assault rate</th>
 		</tr>
-
-<?php
-$con = mysqli_connect('localhost','root','root','Crimes');
-
-$query = mysqli_query($con, "select * from cities");
-while($row=mysqli_fetch_array($query)) {
-
-print_r($row);
-
-
+		<?php
+ 		$opit3 = mysqli_query($con, "select * from cities");
+		while($row=mysqli_fetch_array($opit3)) {
 ?>
- <tr>
- 	<td><? = $row['City']; ?></td>
- 	<td><? = $row['Years']; ?></td>
- 	<td><? = $row['Population']; ?></td>
- 	<td><? = $row['Total violent crime']; ?></td>
- 	<td><? = $row['Murder']; ?></td>
- 	<td><? = $row['Legacy rape']; ?></td>
- 	<td><? = $row['Revised rape']; ?></td>
- 	<td><? = $row['Robbery']; ?></td>
- 	<td><? = $row['Assault']; ?></td>
- 	<td><? = $row['Violent crime rate']; ?></td>
- 	<td><? = $row['Murder rate']; ?></td>
- 	<td><? = $row['Legacy rape rate']; ?></td>
- 	<td><? = $row['Revised rape rate']; ?></td>
- 	<td><? = $row['Robbery rate']; ?></td>
- 	<td><? = $row['Assault rate']; ?></td>
+
+
+ 	<td><?php echo $row['city']; ?></td>
+ 	<td><?php echo $row['years']; ?></td>
+ 	<td><?php echo $row['population']; ?></td>
+ 	<td><?php echo $row['violent_crime_total']; ?></td>
+ 	<td><?php echo $row['murder']; ?></td>
+ 	<td><?php echo $row['legacy_rape1']; ?></td>
+ 	<td><?php echo $row['revised_rape2']; ?></td>
+ 	<td><?php echo $row['robbery']; ?></td>
+ 	<td><?php echo $row['assault']; ?></td>
+ 	<td><?php echo $row['violet_crime_rate']; ?></td>
+ 	<td><?php echo $row['murder_rate']; ?></td>
+ 	<td><?php echo $row['legacy_rape_rate1']; ?></td>
+ 	<td><?php echo $row['revised_rape_rate2']; ?></td>
+ 	<td><?php echo $row['robbery_rate']; ?></td>
+ 	<td><?php echo $row['assault_rate']; ?></td> 	
 </tr>
 
-<?php } ?>
-</table> 	
+<?php }} ?>
+	
 
+
+
+
+
+	
+	</table> 
 	
 </body>
 </html>
